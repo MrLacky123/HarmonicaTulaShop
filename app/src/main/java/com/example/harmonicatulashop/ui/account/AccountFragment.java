8d2548e370a78a9ce8baea3ed5ff4a90686b2553
MrLacky123 <dim.dim2014@list.ru>
2023-02-10 13:00:05 +0300

    account fragment

--- app/src/main/java/com/example/harmonicatulashop/ui/account/AccountFragment.java
+++ app/src/main/java/com/example/harmonicatulashop/ui/account/AccountFragment.java
@@ -1,9 +1,35 @@
 package com.example.harmonicatulashop.ui.account;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.harmonicatulashop.databinding.FragmentAccountBinding;
 
 public class AccountFragment extends Fragment {
 
+    private FragmentAccountBinding binding;
 
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
+        AccountViewModel accountViewModel = new ViewModelProvider(this).get(AccountViewModel.class);
+
+        binding = FragmentAccountBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        //final TextView textView = binding.getRoot();
+        //accountViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
+        return root;
+    }
 }
--- app/src/main/java/com/example/harmonicatulashop/ui/account/AccountViewModel.java
+++ app/src/main/java/com/example/harmonicatulashop/ui/account/AccountViewModel.java
@@ -1,9 +1,20 @@
 package com.example.harmonicatulashop.ui.account;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class AccountViewModel extends ViewModel {
 
+    private final MutableLiveData<String> mText;
 
+    public AccountViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is home fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
 
 }
