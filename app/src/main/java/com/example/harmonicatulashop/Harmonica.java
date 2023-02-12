92b109824da151344e57228433bbe4c0ebd2892b
MrLacky123 <dim.dim2014@list.ru>
2023-02-12 13:08:41 +0300

    harmonica

--- app/src/main/java/com/example/harmonicatulashop/Harmonica.java
+++ app/src/main/java/com/example/harmonicatulashop/Harmonica.java
@@ -1,17 +1,22 @@
 package com.example.harmonicatulashop;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Harmonica {
 
     private final HarmonicaType type;
     private final char tone;
+    private final String color;
     private final List<Integer> range;
+    private final ArrayList<Options> options;
 
-    public Harmonica(HarmonicaType type, char tone, List<Integer> range) {
+    public Harmonica(HarmonicaType type, char tone, String color, List<Integer> range, ArrayList<Options> options) {
         this.type = type;
         this.tone = tone;
+        this.color = color;
         this.range = range;
+        this.options = options;
     }
 
     public HarmonicaType getType() {
@@ -22,7 +27,15 @@ public class Harmonica {
         return tone;
     }
 
+    public String getColor() {
+        return color;
+    }
+
     public List<Integer> getRange() {
         return range;
+    }
+
+    public ArrayList<Options> getOptions() {
+        return options;
     }
 }
