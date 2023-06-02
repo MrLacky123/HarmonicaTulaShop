a39d23afa44fb5150f16504fd73ecc76d3b61846
MrLacky123 <dim.dim2014@list.ru>
2023-06-02 15:36:14 +0300

    catalog changes

--- app/src/main/java/com/example/harmonicatulashop/ui/account/fragments/AddHarmonicaFragment.java
+++ app/src/main/java/com/example/harmonicatulashop/ui/account/fragments/AddHarmonicaFragment.java
@@ -1,5 +1,15 @@
 package com.example.harmonicatulashop.ui.account.fragments;
 
+import static com.example.harmonicatulashop.models.harmonica.Harmonica.ICON;
+import static com.example.harmonicatulashop.models.harmonica.Harmonica.ID;
+import static com.example.harmonicatulashop.models.harmonica.Harmonica.OPTIONS;
+import static com.example.harmonicatulashop.models.harmonica.Harmonica.PRICE;
+import static com.example.harmonicatulashop.models.harmonica.Harmonica.RANGE;
+import static com.example.harmonicatulashop.models.harmonica.Harmonica.TONE;
+import static com.example.harmonicatulashop.models.harmonica.Harmonica.TYPE;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -14,6 +24,7 @@ import android.view.ViewGroup;
 import com.example.harmonicatulashop.MainActivity;
 import com.example.harmonicatulashop.R;
 import com.example.harmonicatulashop.databinding.FragmentAddHarmonicaBinding;
+import com.example.harmonicatulashop.models.harmonica.Harmonica;
 import com.example.harmonicatulashop.ui.account.viewmodels.AddHarmonicaViewModel;
 
 public class AddHarmonicaFragment extends Fragment {
@@ -34,6 +45,9 @@ public class AddHarmonicaFragment extends Fragment {
         binding.setViewModel(viewModel);
         binding.executePendingBindings();
 
+        Bundle bundle = getArguments();
+        bindView(bundle);
+
         viewModel.setBinding(binding);
 
         return binding.getRoot();
@@ -43,5 +57,30 @@ public class AddHarmonicaFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    public void bindView(Bundle bundle) {
+
+        if (bundle == null){
+            return;
+        }
+
+        Harmonica harmonica = new Harmonica(bundle.getByteArray(ICON),
+                bundle.getString(TYPE),
+                bundle.getString(TONE),
+                bundle.getString(RANGE),
+                bundle.getInt(PRICE, 0),
+                bundle.getString(OPTIONS));
+
+        harmonica.setId(bundle.getInt(ID, 0));
+
+        binding.addHarmonicaImage.setImageBitmap(BitmapFactory.decodeByteArray(harmonica.getIcon(), 0, harmonica.getIcon().length));
+        binding.inputHarmonicaType.setText(harmonica.getType());
+        binding.inputHarmonicaTone.setText(harmonica.getTone());
+        binding.inputHarmonicaRange.setText(harmonica.getRange());
+        binding.inputHarmonicaPrice.setText(harmonica.getPrice());
+        binding.inputHarmonicaOptions.setText(harmonica.getOptions());
+        binding.addHarmonicaToCatalog.setText("Сохранить изменения");
+
     }
 }
--- app/src/main/java/com/example/harmonicatulashop/ui/fragments/HarmonicaViewModel.java
+++ app/src/main/java/com/example/harmonicatulashop/ui/fragments/HarmonicaViewModel.java
@@ -1,15 +1,27 @@
 package com.example.harmonicatulashop.ui.fragments;
 
+import static com.example.harmonicatulashop.models.harmonica.Harmonica.ICON;
+import static com.example.harmonicatulashop.models.harmonica.Harmonica.ID;
+import static com.example.harmonicatulashop.models.harmonica.Harmonica.OPTIONS;
+import static com.example.harmonicatulashop.models.harmonica.Harmonica.PRICE;
+import static com.example.harmonicatulashop.models.harmonica.Harmonica.RANGE;
+import static com.example.harmonicatulashop.models.harmonica.Harmonica.TONE;
+import static com.example.harmonicatulashop.models.harmonica.Harmonica.TYPE;
+
+import android.os.Bundle;
 import android.widget.Toast;
 
 import androidx.lifecycle.ViewModel;
 
 import com.example.harmonicatulashop.MainActivity;
+import com.example.harmonicatulashop.R;
 import com.example.harmonicatulashop.database.harmonica.room.cart.CartRepository;
 import com.example.harmonicatulashop.database.harmonica.room.catalog.CatalogRepository;
 import com.example.harmonicatulashop.database.harmonica.room.favourite.FavouriteRepository;
 import com.example.harmonicatulashop.databinding.FragmentHarmonicaBinding;
 import com.example.harmonicatulashop.models.harmonica.Harmonica;
+import com.example.harmonicatulashop.ui.account.fragments.AddHarmonicaFragment;
+import com.example.harmonicatulashop.ui.catalog.fragments.HarmonicaCatalogFragment;
 
 public class HarmonicaViewModel extends ViewModel {
 
@@ -29,6 +41,12 @@ public class HarmonicaViewModel extends ViewModel {
         if (MainActivity.currentAdmin != null) {
 
             new CatalogRepository(MainActivity.INSTANCE.getApplication()).deleteHarmonica(harmonica);
+
+            MainActivity.INSTANCE.setFragment(HarmonicaCatalogFragment.class,
+                    MainActivity.currentLayout, null,
+                    MainActivity.INSTANCE.getResources().getString(R.string.title_harmonica),
+                    null);
+
             return;
         }
 
@@ -41,6 +59,21 @@ public class HarmonicaViewModel extends ViewModel {
     public void addToFavourites() {
 
         if (MainActivity.currentAdmin != null) {
+
+            Bundle bundle = new Bundle();
+
+            bundle.putInt(ID, harmonica.getId());
+            bundle.putByteArray(ICON, harmonica.getIcon());
+            bundle.putString(TYPE, harmonica.getType());
+            bundle.putString(TONE, harmonica.getTone());
+            bundle.putString(RANGE, harmonica.getRange());
+            bundle.putInt(PRICE, harmonica.getPrice());
+            bundle.putString(OPTIONS, harmonica.getOptions());
+
+            MainActivity.INSTANCE.setFragment(AddHarmonicaFragment.class,
+                    R.id.harmonica_layout, bundle,
+                    "Редактировать гармонь",
+                    HarmonicaFragment.class);
 
             return;
         } else if (MainActivity.currentUser == null) {
